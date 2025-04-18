415fb8dba49077871177713dc4f1df92888f87e0
raodz <raodziem@gmail.com>
2025-04-18 15:30:23 +0200

    test: update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,9 @@
+import os
+
 import pytest
 
 
 @pytest.fixture(params=["sample_avi_video.avi", "sample_mp4_video.mp4"])
 def sample_video_path(request):
-    return request.param
+    path = os.path.join("tests", request.param)
+    return path
