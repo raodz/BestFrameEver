aa0a94a888559c0f8967742792b94ad65451e2c3
raodz <raodziem@gmail.com>
2025-05-06 18:51:14 +0200

    feat: update prediction script

--- scripts/predict_best_frame.py
+++ scripts/predict_best_frame.py
@@ -2,13 +2,14 @@ import config
 from src.dataset_preparing.frame_dataset import FrameDataset
 from src.dataset_preparing.frames_list_creator import FramesListCreator
 from src.dataset_preparing.movie import Movie
-from src.prediction.detectors.product_placement_detector import ProductPlacementDetector
 from src.prediction.filters.face_filter import FaceFilter
+from src.prediction.filters.filter_pipeline import FilterPipeline
 from src.prediction.filters.iou_filter import IoUFilter
 from src.prediction.filters.main_char_filter import MainCharacterFilter
 from src.prediction.filters.sharpness_filter import SharpnessFilter
 from src.prediction.final_frame_indicator import FinalFrameIndicator
 from src.prediction.product_placement_blur import ProductPlacementBlur
+from src.prediction.product_placement_recognizer import ProductPlacementRecognizer
 from src.prediction.save_final_frame import save_final_frame
 
 
@@ -21,15 +22,17 @@ def main():
     frames_list = flc.create_frames_list(every_nth_frame=config.filter_every_nth_frame)
     dataset = FrameDataset(frames_list, transform=config.frameset_transforms)
 
+    # Filtering
+
     filters = [FaceFilter, MainCharacterFilter, IoUFilter, SharpnessFilter]
-    filtering_pipeline = FilteringPipeline(filters)
+    filtering_pipeline = FilterPipeline(filters)
     filtered_dataset = filtering_pipeline.filter(dataset)
 
     # Final frame
 
-    final_frame = FinalFrameIndicator(background_filtered_dataset).choose_final_frame()
+    final_frame = FinalFrameIndicator(filtered_dataset).choose_final_frame()
 
-    pp_bbox = ProductPlacementDetector(final_frame).detect_product_placement()
+    pp_bbox = ProductPlacementRecognizer(final_frame).recognize()
     final_frame_no_pp = ProductPlacementBlur(
         final_frame, pp_bbox
     ).blur_product_placement()
