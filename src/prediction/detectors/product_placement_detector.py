7b7c808e00f614a413ef8e7f66738ed39d98891e
raodz <raodziem@gmail.com>
2025-05-06 18:50:46 +0200

    feat: pp recognizer out of detectors (detectors work on datasets, pp recognizer works on one frame)

--- src/prediction/detectors/product_placement_detector.py
+++
@@ -1,7 +0,0 @@
-from src.dataset_preparing.frame_dataset import FrameDataset
-from src.prediction.detectors.base_frame_detector import BaseFrameDetector
-
-
-class ProductPlacementDetector(BaseFrameDetector):
-    def detect(self, dataset: FrameDataset) -> dict[int, bool]:
-        pass
