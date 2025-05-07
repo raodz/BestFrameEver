f4e3e7922b78218a9ed0247b164cd8b8065219cb
raodz <raodziem@gmail.com>
2025-05-07 15:14:12 +0200

    refactor: move calculate_iou to utils.py

--- src/prediction/detectors/iou_detector.py
+++ src/prediction/detectors/iou_detector.py
@@ -5,8 +5,5 @@ from src.prediction.detectors.base_frame_detector import BaseFrameDetector
 
 
 class IoUDetector(BaseFrameDetector):
-    def calculate_iou(self, bbox: tuple[float, float, float, float], frame: np.ndarray):
-        pass
-
     def detect(self, dataset: FrameDataset) -> dict[int, bool]:
         pass
