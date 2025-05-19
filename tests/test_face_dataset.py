5b468931d74210ef343604b6ac62dc1003d9e5e0
raodz <raodziem@gmail.com>
2025-05-19 15:15:22 +0200

    test: improving test_face_dataset.py and adding paths in another file

--- tests/test_face_dataset.py
+++ tests/test_face_dataset.py
@@ -1,43 +1,50 @@
+import pytest
 import torch
+from testing_paths import ANNOTATION_FILE, IMAGE_DIR
 
 from src.dataset_preparing.face_dataset import FaceDataset
 from utils import parse_bbox
 
 
-def test_dataset_length(temp_images_and_annotations):
-    image_dir, annotation_path = temp_images_and_annotations
-    dataset = FaceDataset(images_root=image_dir, annotation_file=annotation_path)
-    assert len(dataset) == 2
+@pytest.fixture
+def face_dataset():
+    return FaceDataset(images_root=IMAGE_DIR, annotation_file=ANNOTATION_FILE)
 
 
-def test_dataset_returns_image_and_boxes(temp_images_and_annotations):
-    image_dir, annotation_path = temp_images_and_annotations
-    dataset = FaceDataset(images_root=image_dir, annotation_file=annotation_path)
+@pytest.fixture
+def image_with_faces(face_dataset):
+    return face_dataset[0]
 
-    image, target = dataset[0]
+
+@pytest.fixture
+def image_without_faces(face_dataset):
+    return face_dataset[1]
+
+
+def test_dataset_length(face_dataset):
+    assert len(face_dataset) == 2
+
+
+def test_dataset_returns_image_and_boxes(image_with_faces):
+    image, target = image_with_faces
 
     assert isinstance(image, torch.Tensor)
     assert image.shape[0] == 3  # RGB
     assert "boxes" in target
     assert isinstance(target["boxes"], torch.Tensor)
-    assert target["boxes"].shape == (2, 4)  # 2 boksy, każdy x1,y1,x2,y2
+    assert target["boxes"].shape == (2, 4)
 
 
-def test_dataset_empty_boxes(temp_images_and_annotations):
-    image_dir, annotation_path = temp_images_and_annotations
-    dataset = FaceDataset(images_root=image_dir, annotation_file=annotation_path)
-
-    _, target = dataset[1]
-    assert target["boxes"].numel() == 0  # brak bboxów
+def test_dataset_empty_boxes(image_without_faces):
+    _, target = image_without_faces
+    assert target["boxes"].numel() == 0
 
 
-def test_dataset_bbox_values(temp_images_and_annotations):
-    image_dir, annotation_path = temp_images_and_annotations
-    dataset = FaceDataset(images_root=image_dir, annotation_file=annotation_path)
+def test_dataset_bbox_values(image_with_faces):
+    _, target = image_with_faces
 
-    _, target = dataset[0]
-    expected_boxes = torch.tensor(
-        [parse_bbox(10, 20, 30, 40), parse_bbox(50, 60, 20, 10)], dtype=torch.float32
-    )
+    actual_boxes = target["boxes"].tolist()
 
-    assert torch.allclose(target["boxes"], expected_boxes)
+    expected_boxes = [parse_bbox(10, 20, 30, 40), parse_bbox(50, 60, 20, 10)]
+
+    assert actual_boxes == expected_boxes
---
+++ tests/testing_paths.py
@@ -0,0 +1,2 @@
+IMAGE_DIR = "test_data"
+ANNOTATION_FILE = "test_data/annotations.txt"
