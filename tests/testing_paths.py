ff9dbb78386e987c983b3312a0ca37f588e431a5
raodz <raodziem@gmail.com>
2025-05-19 19:38:54 +0200

    fix: absolute testing paths

--- tests/testing_paths.py
+++ tests/testing_paths.py
@@ -1,2 +1,5 @@
-IMAGE_DIR = "test_data"
-ANNOTATION_FILE = "test_data/annotations.txt"
+import os
+
+BASE_TEST_DIR = os.path.dirname(os.path.abspath(__file__))
+ANNOTATION_FILE = os.path.join(BASE_TEST_DIR, "test_data", "annotations.txt")
+IMAGE_DIR = os.path.join(BASE_TEST_DIR, "test_data")
