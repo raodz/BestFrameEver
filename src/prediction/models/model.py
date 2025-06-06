2b4892dbb2e24cb8a08c4844dab14691383191f3
jakub1090cn <jakubcymerman505@gmail.com>
2025-06-06 18:13:49 +0200

    chore: move preprocess method to dedicated class

--- src/prediction/models/model.py
+++ src/prediction/models/model.py
@@ -9,6 +9,7 @@ from src.constants import N_BOX_COORDS
 from src.prediction.models.base_detection_head import BaseDetectionHead
 from src.prediction.models.base_detector import BaseDetector
 from src.prediction.models.base_feature_extractor import BaseFeatureExtractor
+from src.prediction.models.postprocessor import Postprocessor
 from src.prediction.models.preprocessor import Preprocessor
 from utils import nms, select_device
 
@@ -102,10 +103,9 @@ class Detector(BaseDetector):
         detection_head_input_feature_channels: int,
         detection_head_hidden_size: int,
         detection_head_leaky_relu_slope: float,
+        # Preprocessor and Postprocessor objects
         preprocessor: Preprocessor,
-        # Postprocess params
-        postprocessing_conf_threshold: float,
-        postprocessing_iou_threshold: float,
+        postprocessor: Postprocessor,
         # Device
         device: str,
     ):
@@ -115,9 +115,8 @@ class Detector(BaseDetector):
         self.num_classes = num_classes
         self.grid_size = grid_size
         self.device = select_device(device)
-        self.conf_threshold = postprocessing_conf_threshold
-        self.iou_threshold = postprocessing_iou_threshold
         self.preprocessor = preprocessor
+        self.postprocessor = postprocessor
 
         self.feature_extractor = self._init_feature_extractor(
             grid_size=self.grid_size,
@@ -157,42 +156,6 @@ class Detector(BaseDetector):
 
     def preprocess(self, x):
         return self.preprocessor(x).to(self.device)
-
-    def _postprocess_output(
-        self, output: torch.Tensor, img_size: tuple[int, int]
-    ) -> tuple:
-        img_w, img_h = img_size
-        device = output.device
-
-        # N_BOX_COORDINATES + confidence_score + n_classes
-        output_dim = N_BOX_COORDS + 1 + self.num_classes
-        output = output.view(
-            -1, self.grid_size, self.grid_size, self.num_boxes, output_dim
-        )
-
-        grid_x = torch.arange(self.grid_size, device=device).view(
-            1, 1, self.grid_size, 1
-        )
-        grid_y = torch.arange(self.grid_size, device=device).view(
-            1, self.grid_size, 1, 1
-        )
-
-        box_xy = torch.sigmoid(output[..., :2])
-        box_wh = torch.exp(output[..., 2:4])
-        box_conf = torch.sigmoid(output[..., 4:5])
-        class_probs = torch.softmax(output[..., 5:], dim=-1)
-
-        x = (box_xy[..., 0] + grid_x) / self.grid_size * img_w
-        y = (box_xy[..., 1] + grid_y) / self.grid_size * img_h
-        w = box_wh[..., 0] * img_w
-        h = box_wh[..., 1] * img_h
-
-        boxes = torch.stack([x - w / 2, y - h / 2, x + w / 2, y + h / 2], dim=-1)
-
-        class_scores, class_ids = torch.max(class_probs, dim=-1)
-        scores = box_conf.squeeze(-1) * class_scores
-
-        return boxes.view(-1, 4), scores.view(-1), class_ids.view(-1)
 
     def predict(
         self,
---
+++ src/prediction/models/postprocessor.py
@@ -0,0 +1,44 @@
+import torch
+
+from src.constants import N_BOX_COORDS
+
+
+class Postprocessor:
+    def __init__(self, num_boxes: int, num_classes: int, grid_size: int):
+        self.num_boxes = num_boxes
+        self.num_classes = num_classes
+        self.grid_size = grid_size
+
+    def __call__(self, output: torch.Tensor, img_size: tuple[int, int]):
+        img_w, img_h = img_size
+        device = output.device
+
+        # N_BOX_COORDINATES + confidence_score + n_classes
+        output_dim = N_BOX_COORDS + 1 + self.num_classes
+        output = output.view(
+            -1, self.grid_size, self.grid_size, self.num_boxes, output_dim
+        )
+
+        grid_x = torch.arange(self.grid_size, device=device).view(
+            1, 1, self.grid_size, 1
+        )
+        grid_y = torch.arange(self.grid_size, device=device).view(
+            1, self.grid_size, 1, 1
+        )
+
+        box_xy = torch.sigmoid(output[..., :2])
+        box_wh = torch.exp(output[..., 2:4])
+        box_conf = torch.sigmoid(output[..., 4:5])
+        class_probs = torch.softmax(output[..., 5:], dim=-1)
+
+        x = (box_xy[..., 0] + grid_x) / self.grid_size * img_w
+        y = (box_xy[..., 1] + grid_y) / self.grid_size * img_h
+        w = box_wh[..., 0] * img_w
+        h = box_wh[..., 1] * img_h
+
+        boxes = torch.stack([x - w / 2, y - h / 2, x + w / 2, y + h / 2], dim=-1)
+
+        class_scores, class_ids = torch.max(class_probs, dim=-1)
+        scores = box_conf.squeeze(-1) * class_scores
+
+        return boxes.view(-1, 4), scores.view(-1), class_ids.view(-1)
