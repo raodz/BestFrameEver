fff457e6decee135058a3a5022caae21c0194ec8
jakub1090cn <jakubcymerman505@gmail.com>
2025-07-12 07:36:05 +0200

    test: change paths to video after merge

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,7 +14,9 @@ from src.utils.paths import KEY_PATH
 has_key = os.path.exists(KEY_PATH)
 
 
-@pytest.fixture(params=["sample_avi_video.avi", "sample_mp4_video.mp4"])
+@pytest.fixture(
+    params=["testing_data/sample_avi_video.avi", "testing_data/sample_mp4_video.mp4"]
+)
 def sample_video_path(request):
     return os.path.join(os.path.dirname(__file__), request.param)
 
